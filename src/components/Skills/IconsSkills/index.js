00445cc63e0a180d4b653f25d6269c7161f2c831
MeReda <medredaben14@gmail.com>
2023-02-11 10:39:19 +0100

    adjusting skills icons

--- src/components/Skills/IconsSkills/index.js
+++ src/components/Skills/IconsSkills/index.js
@@ -18,92 +18,68 @@ import {
 import { faDatabase, faKeyboard } from '@fortawesome/free-solid-svg-icons'
 
 const IconsSkills = () => {
+  const feIcons = [
+    { icon: faHtml5, text: 'HTML 5', iconColor: '#e34f26' },
+    { icon: faCss3Alt, text: 'CSS 3', iconColor: '#0099e5' },
+    { icon: faJs, text: 'JavaScript ES6', iconColor: '#f7df1e' },
+    { icon: faBootstrap, text: 'Bootstrap5', iconColor: '#8310fa' },
+    { icon: faSass, text: 'Sass', iconColor: '#cc6699' },
+    { icon: faReact, text: 'ReactJs', iconColor: '#00d8ff' },
+  ]
+
+  const beIcons = [
+    { icon: faPhp, text: 'Php', iconColor: '#4f5b93' },
+    { icon: faLaravel, text: 'Laravel', iconColor: '#f55247' },
+    { icon: faDatabase, text: 'MySQL & MongoDB', iconColor: '#f29111' },
+    { icon: faPython, text: 'Python', iconColor: '#ffde57' },
+  ]
+
+  const aIcons = [
+    { icon: faGitAlt, text: 'Git', iconColor: '#fc6d26' },
+    { icon: faGithub, text: 'GitHub', iconColor: '#171515' },
+    { icon: faLinux, text: 'Linux', iconColor: '#ffde57' },
+    { icon: faKeyboard, text: 'Touch Typing', iconColor: '#555' },
+    { icon: faFigma, text: 'Figma', iconColor: '#ae4dff' },
+  ]
+
   return (
     <>
       <div className="row skill-container gap-5 justify-content-center w-100">
         <div className="col-md-5 m-3 text-center skills-group">
           <span className="skills-title">Front End</span>
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faHtml5}
-            color="#e34f26"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faCss3Alt}
-            color="#0099e5"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faJs}
-            color="#f7df1e"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faBootstrap}
-            color="#8310fa"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faSass}
-            color="#cc6699"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faReact}
-            color="#00d8ff"
-          />
+          {feIcons.map(({ icon, text, iconColor }, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={icon}
+              className="skills-icon p-2"
+              title={text}
+              color={iconColor}
+            />
+          ))}
         </div>
         <div className="col-md-5 m-3 text-center skills-group">
           <span className="skills-title">Back end</span>
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faPhp}
-            color="#4f5b93"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faLaravel}
-            color="#f55247"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faDatabase}
-            color="#f29111"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faPython}
-            color="#ffde57"
-          />
+          {beIcons.map(({ icon, text, iconColor }, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={icon}
+              className="skills-icon p-2"
+              title={text}
+              color={iconColor}
+            />
+          ))}
         </div>
         <div className="col-md-6 mt-5 text-center skills-group">
           <span className="skills-title">Others</span>
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faGitAlt}
-            color="#fc6d26"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faGithub}
-            color="#171515"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faLinux}
-            color="#ffde57"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faKeyboard}
-            color="#222"
-          />
-          <FontAwesomeIcon
-            className="skills-icon p-2"
-            icon={faFigma}
-            color="#ae4dff"
-          />
+          {aIcons.map(({ icon, text, iconColor }, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={icon}
+              className="skills-icon p-2"
+              title={text}
+              color={iconColor}
+            />
+          ))}
         </div>
       </div>
     </>
